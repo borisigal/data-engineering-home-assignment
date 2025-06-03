ac5ff4b1adbc927c0f73e676fb4b8a9f3dcb3ebd
Barry Sigal <borisigal@gmail.com>
2025-06-03 20:00:06 +0300

    first commit

--- create-update-stack.sh
+++ create-update-stack.sh
@@ -18,10 +18,12 @@ fi
 echo "AWS_ACCESS_KEY_ID: $AWS_ACCESS_KEY_ID"
 echo "AWS_SECRET_ACCESS_KEY: $AWS_SECRET_ACCESS_KEY"
 echo "STACK_NAME: $STACK_NAME"
+echo "AWS_REGION: $AWS_REGION"
 
 # Set AWS credentials as environment variables
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+export AWS_REGION=$AWS_REGION
 
 
 # Notify the user that the stack update process is starting
